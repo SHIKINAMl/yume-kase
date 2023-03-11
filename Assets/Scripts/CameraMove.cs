d18b59cd0af1a7a539fc30671465b97e3440bcab
SHIKINAMl <107552771+SHIKINAMl@users.noreply.github.com>
2023-03-11 19:27:58 +0900

    各部屋の面の数を変更できるように変更（StageManagerから指定）

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -40,9 +40,8 @@ public class CameraMove : MonoBehaviour
 
         if (pos.x == 0)
         {
-            pos.x = 60;
+            pos.x = (stagemanager.sideList[(int)this.transform.position.y/20] - 1) * 20;
         }
-
         else
         {
             pos.x -= 20;
@@ -55,11 +54,10 @@ public class CameraMove : MonoBehaviour
     {
         Vector3 pos = this.transform.position;
 
-        if (pos.x == 60)
+        if (pos.x == (stagemanager.sideList[(int)this.transform.position.y/20] - 1) * 20)
         {
             pos.x = 0;
         }
-
         else
         {
             pos.x += 20;
