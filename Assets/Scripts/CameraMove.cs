987586fccd976f9aa2ecb48c51d9f15a96559105
Kossy3 <103818564+Kossy3@users.noreply.github.com>
2023-04-09 04:52:22 +0900

    BGMループの実装

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -15,22 +15,6 @@ public class CameraMove : MonoBehaviour
     public void Start()
     {
         stagemanager = GameObject.Find("StageManager").GetComponent<StageManager>();
-        BGMList = stagemanager.BGMList;
-        GetComponent<AudioSource>().PlayOneShot(BGMList[0].BGM);
-    }
-
-    public void Update()
-    {
-        for (var i = 1; i < BGMList.Length; i++)
-        {
-            if (lastTimeFlagList != currentFlagList)
-            {
-                GetComponent<AudioSource>().PlayOneShot(BGMList[i].BGM);
-            }
-        }
-
-        lastTimeFlagList = currentFlagList;
-        currentFlagList = (from i in BGMList select stagemanager.GetFlagByName(i.flagName)).ToArray();
     }
 
     public void OnClickToLeft()
