a033bc170f4fd9a57cd31ae171cf5a0868f106b0
Kossy3 <103818564+Kossy3@users.noreply.github.com>
2023-04-09 01:37:56 +0900

    一通りプレイできるところまで

--- Assets/Scripts/school/EscapeLoop.cs
+++ Assets/Scripts/school/EscapeLoop.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class EscapeLoop : MonoBehaviour
 {
@@ -11,11 +12,16 @@ public class EscapeLoop : MonoBehaviour
     List<string> moveHistory = new List<string>();
     [SerializeField]
     string flagname;
-    int orderlength; 
+    int orderlength;
+    [SerializeField]
+    bool debug;
     // Start is called before the first frame update
     void Start()
     {
         orderlength = flagsOrder.Count;
+        for (int i=0; i<orderlength; i++){
+            moveHistory.Add("blank");
+        }
         stageManager = GameObject.Find("StageManager").GetComponent<StageManager>();
     }
 
@@ -26,11 +32,13 @@ public class EscapeLoop : MonoBehaviour
         {
             if (stageManager.GetFlagByName(flag))
             {
-                moveHistory.Add(flag);
+                if (moveHistory[moveHistory.Count-1] != flag){
+                    moveHistory.Add(flag);
+                }
                 stageManager.SetFlagByName(stageManager.eventFlagList, flag, false);
             }
         }
-        if (moveHistory.Count >= 5 && moveHistory.GetRange(orderlength-5, 5) == flagsOrder) {
+        if (moveHistory.GetRange(orderlength-5, 5).SequenceEqual(flagsOrder) || debug) {
             stageManager.SetFlagByName(stageManager.eventFlagList, flagname, true);
         }
         else
