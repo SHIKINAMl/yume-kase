e9c6851570b5ca5be76ef1b7c15fb827c5421b8e
Kossy3 <103818564+Kossy3@users.noreply.github.com>
2023-08-17 00:12:45 +0900

    異臭定期バグ修正

--- Assets/Scripts/stage4/smell.cs
+++ Assets/Scripts/stage4/smell.cs
@@ -8,19 +8,28 @@ public class smell : MonoBehaviour
     public float interval;
     private TextWindow textWindow; 
     private StageManager stageManager;
+    private bool smellFlag;
     // Start is called before the first frame update
     void Start()
     {
+        smellFlag = true;
         textWindow = GameObject.Find("TextWindow").GetComponent<TextWindow>();
         stageManager = GameObject.Find("StageManager").GetComponent<StageManager>();
         StartCoroutine(smellwww());
     }
 
+    void FixedUpdate()
+    {
+        if (stageManager.GetFlagByName("換気扇1")){
+            smellFlag = false;
+        }
+    }
+
     // Update is called once per frame
     private IEnumerator smellwww()
     {
-        if (!stageManager.GetFlagByName("換気扇1")){
-            yield return new WaitForSeconds(interval);
+        yield return new WaitForSeconds(interval);
+        if (smellFlag){    
             stageManager.SetFlagByName(stageManager.eventFlagList, "もや発生", true);
             StartCoroutine(smellwww());
         }
